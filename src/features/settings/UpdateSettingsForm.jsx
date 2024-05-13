52a08e8bb855fea7dd146c19e04210cc3e207ee3
Fran <francorossi.visual@gmail.com>
2024-05-13 20:31:51 -0300

    started working on bookings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,10 +1,10 @@
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
-import Spinner from "../../ui/Spinner";
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
+import Spinner from '../../ui/Spinner';
 
-import { useSettings } from "./useSettings";
-import useUpdateSetting from "./useUpdateSetting";
+import { useSettings } from './useSettings';
+import useUpdateSetting from './useUpdateSetting';
 
 function UpdateSettingsForm() {
   const {
@@ -35,7 +35,7 @@ function UpdateSettingsForm() {
           type='number'
           id='min-nights'
           defaultValue={minBookingLength}
-          onBlur={(e) => handleUpdate(e, "minBookingLength")}
+          onBlur={(e) => handleUpdate(e, 'minBookingLength')}
           disabled={isUpdatingSetting}
         />
       </FormRow>
@@ -44,7 +44,7 @@ function UpdateSettingsForm() {
           type='number'
           id='max-nights'
           defaultValue={maxBookingLength}
-          onBlur={(e) => handleUpdate(e, "maxBookingLength")}
+          onBlur={(e) => handleUpdate(e, 'maxBookingLength')}
           disabled={isUpdatingSetting}
         />
       </FormRow>
@@ -53,7 +53,7 @@ function UpdateSettingsForm() {
           type='number'
           id='max-guests'
           defaultValue={maxGuestsPerBooking}
-          onBlur={(e) => handleUpdate(e, "maxGuestsPerBooking")}
+          onBlur={(e) => handleUpdate(e, 'maxGuestsPerBooking')}
           disabled={isUpdatingSetting}
         />
       </FormRow>
@@ -62,11 +62,10 @@ function UpdateSettingsForm() {
           type='number'
           id='breakfast-price'
           defaultValue={breakfastPrice}
-          onBlur={(e) => handleUpdate(e, "breakfastPrice")}
+          onBlur={(e) => handleUpdate(e, 'breakfastPrice')}
           disabled={isUpdatingSetting}
         />
       </FormRow>
-      <button></button>
     </Form>
   );
 }
